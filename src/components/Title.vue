01205ceb87315c044a66a94fc6e5eee1e468872b
lexbert17 <lexbertlazo@gmail.com>
2023-11-22 14:01:51 +0100

    add: Title component

---
+++ src/components/Title.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    data() {
+        return {
+            text: "La mia prima app con vite"
+        };
+    },
+};
+</script>
+
+<template>
+    <h2>{{ text }}</h2>
+</template>
+
+<style>
+
+</style>
